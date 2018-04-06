27bf49c93065a41288b432107a11fadb26e058e3
Unknown <j.zambre66@gmail.com>
2018-04-06 11:29:01 +0800

    Updated code

--- src/org/javabrains/jayendra/dto/UserDetails.java
+++ src/org/javabrains/jayendra/dto/UserDetails.java
@@ -1,15 +1,31 @@
 package org.javabrains.jayendra.dto;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 @Entity
 @Table(name="USER_DETAILS")
@@ -22,9 +38,6 @@ public class UserDetails {
 	@Column(name="USERNAME")
 	private String usedrName;
 	
-	@Column(name = "ADDRESS")
-	private String address;
-	
 	@Temporal(TemporalType.DATE)
 	@Column(name="JOINEDDATE")
 	private Date joinedDate;
@@ -33,12 +46,50 @@ public class UserDetails {
 	@Column(name="DESCRIPTION")
 	private String description;
 	
-	public String getAddress() {
-		return address;
+	@ElementCollection(fetch= FetchType.EAGER)
+	@JoinTable(name="USER_ADDRESS", joinColumns=@JoinColumn(name="USER_ID"))
+	@GenericGenerator(name="hilo-gen", strategy="hilo")
+	@CollectionId(columns = { @Column(name="ADDRESS_ID") }, generator = "hilo-gen", type= @Type(type="long"))
+	private Collection<Address> listOfAddresses = new ArrayList<>();
+	
+	public Collection<Address> getListOfAddresses() {
+		return listOfAddresses;
 	}
-	public void setAddress(String address) {
-		this.address = address;
+
+	public void setListOfAddresses(ArrayList<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
 	}
+//	@Embedded
+//	@AttributeOverrides({
+//	@AttributeOverride (name= "street", column= @Column(name= "HOME_STREET")),
+//	@AttributeOverride (name= "city", column= @Column(name= "HOME_CITY")),
+//	@AttributeOverride (name= "state", column= @Column(name= "HOME_STATE")),
+//	@AttributeOverride (name= "pincode", column= @Column(name="HOME_PINCODE"))
+//	})
+//	private Address homeAddress;
+	
+//	public Address getHomeAddress() {
+//		return homeAddress;
+//	}
+//
+//	public void setHomeAddress(Address homeAddress) {
+//		this.homeAddress = homeAddress;
+//	}
+//	@Embedded
+//	private Address officeAddress;
+//	
+//	public Address getOfficeAddress() {
+//		return officeAddress;
+//	}
+//
+//	public void setOfficeAddress(Address officeAddress) {
+//		this.officeAddress = officeAddress;
+//	}
+
+	public void setListOfAddresses(Collection<Address> listOfAddresses) {
+		this.listOfAddresses = listOfAddresses;
+	}
+
 	public Date getJoinedDate() {
 		return joinedDate;
 	}
--- src/org/jayendra/hibernate/HibernateTest.java
+++ src/org/jayendra/hibernate/HibernateTest.java
@@ -2,9 +2,13 @@ package org.jayendra.hibernate;
 
 import java.util.Date;
 
+import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.classic.Session;
+import org.hibernate.hql.ast.util.NodeTraverser.VisitationStrategy;
+import org.javabrains.jayendra.dto.Address;
 import org.javabrains.jayendra.dto.UserDetails;
 
 public class HibernateTest {
@@ -12,9 +16,28 @@ public class HibernateTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDetails user = new UserDetails();
+		
+		Address address = new Address();
+		address.setCity("Singapore");
+		address.setState("Singapore");
+		address.setStreet("Buona Vista road");
+		address.setPincode("123456");
+		
+		Address address2 = new Address();
+		address2.setStreet("Serangoon street");
+		address2.setCity("Fairfax");
+		address2.setState("Washington");
+		address2.setPincode("987654");
+		
 		user.setUserId(1);
 		user.setUsedrName("First Name");
-		user.setAddress("Clementi");
+		
+		user.getListOfAddresses().add(address);
+		user.getListOfAddresses().add(address2);
+		
+//		user.setHomeAddress(address);
+//		user.setOfficeAddress(address2);
+		
 		user.setJoinedDate(new Date());
 		user.setDescription("Description goes here");
 		
@@ -23,6 +46,14 @@ public class HibernateTest {
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit();
+		
+		user = null;
+		session = sessionFactory.openSession();
+		
+		user = (UserDetails) session.get(UserDetails.class, 1);
+		session.close();
+		System.out.println(user.getListOfAddresses().size());
+		
 		
 	}
 
